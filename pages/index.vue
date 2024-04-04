0f4f7fd61bd081b0dab7ee4b7581f999e44333fa
Xraykill115 <Xraykill1.xray@gmail.com>
2024-04-04 10:52:49 +0200

    finished the first version if the index page

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,11 @@
 <p>I am Jérémi, a passionate IT engineer from Auxerre, a small city located in Bourgogne Franche-Comté, France.</p>
 <p>Freshly graduated from EPSI engineering school and with three years of experiences, i wish upon putting my passion to use in a company.</p>
 <p>I am fairly familiar with quite a number of tools and languages, as i like to think variety is a strong point of the work of developper, however, i think my favorite technologies so far are JavaScript (and his frameworks) and Python.</p>
+<p>I am also very fond of cybersecurity, a specialty i wish to dive into!</p>
 <p>If you are looking for a young motivated software engineer feel free to contact me!</p>
 <h2>Where you are</h2>
-<p>You are actually visiting my portfolio, a place where i expose my (non confidential) works and projects.</p>
+<p>You are actually visiting my portfolio, a place where i expose my (non confidential) work and projects.</p>
+<p>This website itself was made by my hands with Nuxt, a framework for JavaScript as a personnal project to learn and master Nuxt, a framework i particularly affectionnate.</p>
+<p>You probably already noticed, but thi website is hosted on github, a tool i am quite proefficient with due my years using it has a professionnal.</p>
+<p>Github Pages was my tool of choice when i decided to do a portfolio, and if you have any suggestions about it, i am fully listening.</p>
 </template>
