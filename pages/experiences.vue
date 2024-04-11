0b27e2b793e7532f1880ed66e5d3f6a3c31f34de
Xraykill115 <Xraykill1.xray@gmail.com>
2024-04-11 11:55:02 +0200

    added first version of experiences page

--- pages/experiences.vue
+++ pages/experiences.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    
+    <h1>Professional experience</h1>
+    <h2>Developer consultant</h2>
+    <h3>From 2021 to 2023 at Quartz Insight, Paris (full remote)</h3>
+    <hr />
+    <h2>Software engineer</h2>
+    <h3>From 2020 to 2021 at Indusa Electronics, Auxerre</h3>
+    <hr />
+    <h2>Intern</h2>
+    <h3>From May 2019 to June 2019 at IT Service of mairie d'Auxerre</h3>
+    <hr />
+    <h2>Intern</h2>
+    <h3>February 2015 at IT Service of mairie d'Auxerre</h3>
   </div>
 </template>
 
